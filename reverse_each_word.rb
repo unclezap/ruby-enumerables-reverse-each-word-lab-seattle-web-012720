25d846450c25882437ddfddc3f267a1e42fbfc9c
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-21 13:18:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,23 +1,23 @@
-def reverse_each_word(string)
-  new = string.reverse
-  array = []
-  array.collect
-  array = new.split
-  array = array.reverse
-  array = array*" "
-  return array
-end 
-
 #def reverse_each_word(string)
- #new = string.reverse
- #array = []
- #array = new.split
- #array = array.reverse
- #result = ""
- 
-# result = array. { |word| "#{word} asdfasdf"}
- 
+ # new = string.reverse
+  #array = []
+  #array.collect
+  #array = new.split
+  #array = array.reverse
+  #array = array*" "
+  #return array
+#end 
 
+def reverse_each_word(string)
+ new = string.reverse
+ array = []
+ result = []
+ array = new.split
+ array = array.reverse
+ result = array.collect do |word|
+   word = "#{word} asdfsad"
+ end
+ array = array*" "
+ return array
 
-
-#end
+end
